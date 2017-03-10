4de0b67cd9081e5976fea60c32c233d6bd4b21bb
ФО-240003 Кобельков Иннокентий Денисович <anamunesisu@yandex.ru>
2017-03-10 13:52:00 +0500

    checking for numbers and letters

--- Lecture 1/2/script.js
+++ Lecture 1/2/script.js
@@ -5,15 +5,15 @@ function formCheck() {
 	for (var i = 0; i < x.length; i++) {
 		x.elements[i].className = "";
 	}
-   
-  if (!x.elements[5].value.endsWith(".jpg")) {
+
+	if (!x.elements[5].value.endsWith(".jpg")) {
 		text += "Изображение должно быть формата *.jpg" + "<br>";
     x.elements[5].className = "error";
 	}
-  
+
 	if (x.elements[5].files[0].size > 1038336) {
 		text += "Размер фото должен быть меньше 1 Мб" + "<br>";
 	}
-  
+
 	document.getElementById("errorBlock").innerHTML = text;
 }
